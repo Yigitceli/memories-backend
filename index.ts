5f530183f2f181a144d13b1e5068762c07970474
Cemal Yiğit <71134169+Yigitceli@users.noreply.github.com>
2023-02-02 21:12:59 +0300

    cors uptdate

--- index.ts
+++ index.ts
@@ -9,7 +9,7 @@ import "./firebaseInıt";
 dotenv.config();
 const port = process.env.PORT;
 const app: Express = express();
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: "https://sprightly-quokka-17d6fa.netlify.app" }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api", indexRouter);
